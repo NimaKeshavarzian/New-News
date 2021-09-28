942d26bd463e35e6746b9f2f5ea88388dd62792e
NimaKeshavarzian <nimakeshavarzian@gmail.com>
2021-09-28 14:07:27 +0330

    Search Box

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,13 @@ window.onload = function() {
     toggleShowMode(document.getElementById('loader'));
 }
 
+// Search box
+var searchBoxTransmitter = document.getElementById("searchBoxTransmitter");
+var searchBox = document.getElementById("searchBox");
+var closeSearchBoxBtn = document.getElementById("closeSearchBox");
+searchBoxTransmitter.onclick = function() { toggleShowMode(searchBox) };
+closeSearchBoxBtn.onclick = function() { toggleShowMode(searchBox) };
+
 // Toggle elements show mode
 function toggleShowMode(elem) {
     elem.classList.toggle('hidden')
