bfa1b6b84246354aca557ab3aaacb045a898ca97
NimaKeshavarzian <nimakeshavarzian@gmail.com>
2021-09-29 13:18:36 +0330

    Responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     // Loader
     toggleShowMode(document.getElementById('loader'));
+    // Menu
+    showMenu();
 }
 
 // Search box
@@ -9,6 +11,22 @@ var searchBox = document.getElementById("searchBox");
 var closeSearchBoxBtn = document.getElementById("closeSearchBox");
 searchBoxTransmitter.onclick = function() { toggleShowMode(searchBox) };
 closeSearchBoxBtn.onclick = function() { toggleShowMode(searchBox) };
+
+// Menu
+window.addEventListener("resize", function() { showMenu() });
+
+function showMenu() {
+    var menuTransmitter = document.getElementById("menuTransmitter");
+    var menu = document.getElementById("pageMenu");
+    var deviceWidth = window.innerWidth;
+    if (deviceWidth <= 420) {
+        toggleShowMode(menuTransmitter);
+        toggleShowMode(menu);
+        menuTransmitter.addEventListener("click", function() { toggleShowMode(menu) });
+    } else {
+        menuTransmitter.classList.add("hidden");
+    }
+}
 
 // Toggle elements show mode
 function toggleShowMode(elem) {
